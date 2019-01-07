ebdd54469ccea365bc4b134826e7703ea7d29309
visafefe <44175579+visafefe@users.noreply.github.com>
2019-01-07 10:39:10 +0000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,7 +2,10 @@
 library(data.table)
 library(dplyr)
 
+## the file will be directly unzipped in the working directory
 chemin <- file.path(getwd(),"exdata_data_household_power_consumption","household_power_consumption.txt")
+
+##table_hous_power_consumption will be the table(dataset) learnt from the file
 table_hous_power_consumption <- fread(chemin)
 table_num_hpc <- as.numeric(table_hous_power_consumption$Global_active_power)
 png('plot1.png', width = 480, height = 480)
